46b68d86a939cbd42e9cb0238e20f20ce3b0dfb0
DaeJungYoon <dbseowjd12@gmail.com>
2024-10-22 10:37:19 +0900

    20241021[feat]forWhile 3단 실습

---
+++ javascript/1022/forWhile.js
@@ -0,0 +1,12 @@
+for (i = 1; i < 9 + 1; i++) {
+  console.log(i * 3);
+}
+console.log('---------for3단-------');
+
+let nums = 0;
+while (nums < 9) {
+  nums++;
+  console.log(nums * 3);
+}
+console.log('---------while3단-------');
+
