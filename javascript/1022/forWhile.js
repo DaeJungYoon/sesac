9d498a3199aaaa9ee27fc79ec2745db6d8d5c17c
DaeJungYoon <dbseowjd12@gmail.com>
2024-10-22 11:22:42 +0900

    20241022[feat]for 출석

--- javascript/1022/forWhile.js
+++ javascript/1022/forWhile.js
@@ -1,12 +1,18 @@
 for (i = 1; i < 9 + 1; i++) {
   console.log(i * 3);
 }
-console.log('---------for3단-------');
+console.log('---------for3 단-------');
 
 let nums = 0;
 while (nums < 9) {
   nums++;
   console.log(nums * 3);
 }
-console.log('---------while3단-------');
+console.log('---------while3 단-------');
 
+const names = ['jun', 'beemo', 'ken', 'lynda'];
+for (let index = 0; index < names.length; index++) {
+  let name = names[index];
+  console.log(index + 1, name);
+}
+console.log('---------for 출석-------');
