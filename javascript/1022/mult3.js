b4ee329e10a0c88080976ecef47c4899a0ec22ab
DaeJungYoon <dbseowjd12@gmail.com>
2024-10-22 16:52:46 +0900

    20241022[feat]40이하의 3단 for문 돌리기

---
+++ javascript/1022/mult3.js
@@ -0,0 +1,11 @@
+for (let i = 3; i <= 40; i++) {
+  if (i % 3 === 0) console.log(i);
+}
+
+for (let i = 3; i <= 40; num += 3) {
+  console.log(i);
+}
+for (let i = 1; i * 3 <= 40; i++) {
+  console.log(i * 3);
+}
+console.log('--------------------');
