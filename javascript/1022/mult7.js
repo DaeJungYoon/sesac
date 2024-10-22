dc177a43d33d8496225ebb27372c8c1ffbecdac6
DaeJungYoon <dbseowjd12@gmail.com>
2024-10-22 20:58:31 +0900

    20241022[feat] continue 사용

--- javascript/1022/mult7.js
+++ javascript/1022/mult7.js
@@ -4,5 +4,37 @@ for (let i = 1; i < 100; i++) {
     newSeven.push(i);
   }
 }
-console.log(newSeven);
 console.log(newSeven.length);
+console.log('---------1--------');
+
+let a = 0;
+for (let i = 7; i <= 100; i += 7) {
+  a++;
+}
+console.log(a);
+console.log('---------2--------');
+
+let count = 0;
+for (let i = 1; i * 7 <= 100; i++) {
+  count++;
+  continue;
+}
+console.log(count);
+console.log('--------3---------');
+
+count = 0;
+for (let i = 1; i <= 100; i++) {
+  if (i % 7) continue;
+  count++;
+}
+console.log(count);
+console.log('--------4---------');
+
+count = 0;
+for (let i = 1; i <= 100; i++) {
+  if (i % 7 === 0) {
+    count++;
+  }
+}
+console.log(count);
+console.log('--------5---------');
