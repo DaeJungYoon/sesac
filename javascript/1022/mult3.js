36d123608fb310f146d35be364be4d325367fb8d
DaeJungYoon <dbseowjd12@gmail.com>
2024-10-22 17:27:55 +0900

    20241022[fix] mult3 오타 수정

--- javascript/1022/mult3.js
+++ javascript/1022/mult3.js
@@ -2,7 +2,7 @@ for (let i = 3; i <= 40; i++) {
   if (i % 3 === 0) console.log(i);
 }
 
-for (let i = 3; i <= 40; num += 3) {
+for (let i = 3; i <= 40; i += 3) {
   console.log(i);
 }
 for (let i = 1; i * 3 <= 40; i++) {
