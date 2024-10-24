868f182c2d6ca5b64f82fb17b8ac46ba6153ef82
DaeJungYoon <dbseowjd12@gmail.com>
2024-10-24 18:54:49 +0900

    20241024[feat] 배열 곱해서 출력

--- 과제/열번째/arr.js
+++ 과제/열번째/arr.js
@@ -1,7 +1,16 @@
 let arr = [1, 3, 5, -3, 9, 10, 23, -6, 44, 22, -10, 5, 20];
 
-const plusN = arr.forEach((num) => {
+const plusNum = arr.forEach((num) => {
   if (num > 0) {
     console.log(num);
   }
 });
+console.log('----------1----------');
+
+const multNum = arr.map((num) => {
+  let mult = num * 2;
+  console.log(mult);
+  return mult;
+});
+console.log('----------2----------');
+
