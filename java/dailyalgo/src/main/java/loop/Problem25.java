05d78175da9b09eb03385e7b5ab02d9a6a433c0c
DaeJungYoon <dbseowjd12@gmail.com>
2024-12-15 21:49:42 +0900

    dailyalgo 25 loop 문제 설명  추가

--- java/dailyalgo/src/main/java/loop/Problem25.java
+++ java/dailyalgo/src/main/java/loop/Problem25.java
@@ -1,11 +1,11 @@
 package loop;
 
 public class Problem25 {
-//    각 자릿수의 합 구하기
+//    모든 약수의 총합 구하기
 //    문제 설명
-//    주어진 자연수 number의 각 자릿수들의 합을 구하는 solution 함수를 작성하시오.
+//    자연수 number가 주어질 때, 1부터 number까지 각 숫자의 모든 약수들의 총합을 구하는 solution 함수를 작성하시오.
 //    제한 사항
-//    number는 1 이상 10^18 이하의 자연수입니다.
+//    number는 1 이상 100 이하의 자연수입니다.
     public static void main(String[] args) {
         System.out.println(Solution.solution(5));
 
