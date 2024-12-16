ee76e4ebbeadbef42d7dc64ba0746985b47b7331
DaeJungYoon <dbseowjd12@gmail.com>
2024-12-16 17:56:02 +0900

    empty

--- java/dailyalgo/src/main/java/arrays/Problem48.java
+++ java/dailyalgo/src/main/java/arrays/Problem48.java
@@ -6,7 +6,7 @@ public class Problem48 {
 //    정수 원소로 이루어진 배열 numbers가 주어질 때, numbers의 원소 중 가장 큰 값을 반환하는 solution 함수를 작성하시오.
 //    제한 사항
 //    numbers의 각 원소는 절댓값이 500 이하인 정수입니다.
-//    numbers는 비어있지 않으며, 길이가 100을 넘지 않습니다.
+//    numbers는 비어있지 않으며, 길이가 100을 넘지 않습니다.dd
     public static void main(String[] args) {
         System.out.println(Solution.solution(new int[]{1, 2, 3, 4}));
     }
