814e310fccee8162ec2c2227c23de9d2c059f645
DaeJungYoon <dbseowjd12@gmail.com>
2024-12-30 15:39:12 +0900

    엔티티 매핑 with lombok

--- java/demo/src/main/java/com/example/demo/myjpasite/PostJpa.java
+++ java/demo/src/main/java/com/example/demo/myjpasite/PostJpa.java
@@ -4,8 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostJpa {
 
     @Id
@@ -17,18 +22,4 @@ public class PostJpa {
     private String content;
     // 여기까지 demodb와 연결될 준비가 됨
 
-    public PostJpa() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getContent() {
-        return content;
-    }
 }
