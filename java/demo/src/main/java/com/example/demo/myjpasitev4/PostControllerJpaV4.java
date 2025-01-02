99c8c8c897d7b3311fc48a07b01561270944cf84
DaeJungYoon <dbseowjd12@gmail.com>
2025-01-02 17:26:29 +0900

    api 응답 구조화

--- java/demo/src/main/java/com/example/demo/myjpasitev4/PostControllerJpaV4.java
+++ java/demo/src/main/java/com/example/demo/myjpasitev4/PostControllerJpaV4.java
@@ -1,13 +1,9 @@
 package com.example.demo.myjpasitev4;
 
-import com.example.demo.myjpasitev4.dto.PostCreateRequestDto;
-import com.example.demo.myjpasitev4.dto.PostListResponseDto;
-import com.example.demo.myjpasitev4.dto.PostResponseDto;
-import com.example.demo.myjpasitev4.dto.PostUpdateRequestDto;
-import lombok.Getter;
+import com.example.demo.myjpasitev4.dto.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,28 +15,54 @@ public class PostControllerJpaV4 {
     private final PostServiceJpaV4 postServiceJpaV4;
 
     @PostMapping
-    public PostResponseDto createPost(@RequestBody PostCreateRequestDto requestDto){
-        return postServiceJpaV4.createPost(requestDto);
+    public ResponseEntity<ApiResponse<PostResponseDto>> createPost(@RequestBody PostCreateRequestDto requestDto){
+        return ResponseEntity
+                .status(HttpStatus.CREATED) // status가 있으면 이것을 보여주는 것이 좋긴함
+                .body(
+                        ApiResponse.ok(
+                                "게시글을 정상적을 작성되었습니다.",
+                                "CREATE",
+                                postServiceJpaV4.createPost(requestDto)
+                        )
+                );
     }
 
     @GetMapping
-    public List<PostListResponseDto> readPosts(){
-        return postServiceJpaV4.readPosts();
+    public ResponseEntity<ApiResponse<List<PostListResponseDto>>> readPosts(){
+        List<PostListResponseDto> data = postServiceJpaV4.readPosts();
+        ApiResponse<List<PostListResponseDto>> response = ApiResponse.ok(data); //message, code, data를 넣기 위해
+        return ResponseEntity.ok(response);
     }
     @GetMapping("/{id}")
-    public PostResponseDto readPostById(@PathVariable Long id){
-        return postServiceJpaV4.readPostById(id);
+    public ResponseEntity<ApiResponse<PostResponseDto>> readPostById(@PathVariable Long id){
+        return ResponseEntity.ok(ApiResponse.ok(postServiceJpaV4.readPostById(id)));
     }
     // PUT id 제목 / 내용 dto data
     @PutMapping("{id}")
-    public PostResponseDto updatePost(@PathVariable Long id, @RequestBody PostUpdateRequestDto requestDto){
-        return postServiceJpaV4.updatePost(id, requestDto);
+    public ResponseEntity<ApiResponse<PostResponseDto>> updatePost(@PathVariable Long id, @RequestBody PostUpdateRequestDto requestDto){
+
+        return ResponseEntity.ok(ApiResponse.ok(postServiceJpaV4.updatePost(id, requestDto)));
+
     }
 
+//    @DeleteMapping("/{id}")
+////    @ResponseStatus(HttpStatus.NO_CONTENT)
+//    public ResponseEntity<Void> deletePost(@PathVariable Long id){
+//        postServiceJpaV4.deletePost(id);
+//        return ResponseEntity.noContent().build();
+//    }
     @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deletePost(@PathVariable Long id){
+//    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<ApiResponse<Void>> deletePost(@PathVariable Long id){
         postServiceJpaV4.deletePost(id);
+        return ResponseEntity.ok(
+                ApiResponse
+                        .ok(
+                                "게시글 삭제되었습니다",
+                                "DELETE",
+                                null
+                        )
+        );
     }
 
 }
