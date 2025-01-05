b4b5fc4530d6c2b67a0acfdba3df33034f24b3a0
DaeJungYoon <dbseowjd12@gmail.com>
2025-01-05 22:25:34 +0900

    empty

--- java/demo/src/main/java/com/example/demo/myjpasitev4/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.myjpasitev4.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(){
-        super("리소스를 찾을 수 없습니다.");
-    }
-}
