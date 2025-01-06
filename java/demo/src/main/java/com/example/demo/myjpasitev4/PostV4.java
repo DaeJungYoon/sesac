bce0f6e1debaa005dbc033241b6383ef951b2f3d
DaeJungYoon <dbseowjd12@gmail.com>
2025-01-06 10:40:21 +0900

    @Column nullable false 설정

--- java/demo/src/main/java/com/example/demo/myjpasitev4/PostV4.java
+++ java/demo/src/main/java/com/example/demo/myjpasitev4/PostV4.java
@@ -1,10 +1,7 @@
 package com.example.demo.myjpasitev4;
 
 import com.example.demo.myjpasitev4.dto.PostUpdateRequestDto;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -18,6 +15,7 @@ public class PostV4 extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String title;
     private String content;
     private String author;
