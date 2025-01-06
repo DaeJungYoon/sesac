85e85003f3be4513a3ea09c4d5137150980ea078
DaeJungYoon <dbseowjd12@gmail.com>
2025-01-06 17:57:02 +0900

    UserResponseDto 구현

---
+++ java/whatisinyourmind/src/main/java/com/example/whatisinyourmind/dto/response/UserResponseDto.java
@@ -0,0 +1,37 @@
+package com.example.whatisinyourmind.dto.response;
+
+import com.example.whatisinyourmind.User;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class UserResponseDto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private final Long id;
+
+    private final String username;
+    private final String email;
+    private final String nickname;
+    private final int age;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime updateAt;
+
+    public static UserResponseDto from(User entity){
+        return UserResponseDto.builder()
+                .id(entity.getId())
+                .username(entity.getUsername())
+                .nickname(entity.getNickname())
+                .age(entity.getAge())
+                .createdAt(entity.getCreatedAt())
+                .updateAt(entity.getUpdatedAt())
+                .build();
+    }
+}
