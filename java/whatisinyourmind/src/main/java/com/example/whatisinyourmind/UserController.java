c95ce46de10d3f371409e21cb36504a5bb203f64
DaeJungYoon <dbseowjd12@gmail.com>
2025-01-06 17:21:32 +0900

    코드 정리

--- java/whatisinyourmind/src/main/java/com/example/whatisinyourmind/UserController.java
+++ java/whatisinyourmind/src/main/java/com/example/whatisinyourmind/UserController.java
@@ -1,24 +1,5 @@
 package com.example.whatisinyourmind;
 
-import jakarta.persistence.Column;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@NoArgsConstructor
 public class UserController {
-    @Column(nullable = false)
-    @NotBlank
-    private String username;
 
-    @Column(nullable = false)
-    private String email;
-
-    @Column(nullable = false)
-    private String nickname;
-
-    private int age;
-
-    @Column(nullable = false)
-    private boolean isActive;
 }
