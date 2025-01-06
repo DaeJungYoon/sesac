63e6259a0da9d3180f246d32a246ef342eb99d97
DaeJungYoon <dbseowjd12@gmail.com>
2025-01-06 17:18:30 +0900

    validation dependencies 추가

--- java/whatisinyourmind/src/main/java/com/example/whatisinyourmind/UserController.java
+++ java/whatisinyourmind/src/main/java/com/example/whatisinyourmind/UserController.java
@@ -1,4 +1,24 @@
 package com.example.whatisinyourmind;
 
+import jakarta.persistence.Column;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class UserController {
+    @Column(nullable = false)
+    @NotBlank
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    private int age;
+
+    @Column(nullable = false)
+    private boolean isActive;
 }
