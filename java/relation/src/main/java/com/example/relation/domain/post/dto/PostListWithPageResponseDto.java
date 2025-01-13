fc200e095e8ce949e99a55b08d979eebf3f735d3
DaeJungYoon <dbseowjd12@gmail.com>
2025-01-13 13:48:49 +0900

    class 긁어와서 사용

--- java/relation/src/main/java/com/example/relation/domain/post/dto/PostListWithPageResponseDto.java
+++ java/relation/src/main/java/com/example/relation/domain/post/dto/PostListWithPageResponseDto.java
@@ -6,12 +6,13 @@ import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.domain.Page;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
 @Builder
 public class PostListWithPageResponseDto {
-    private List<PostListResponseDto> posts;
+    private List<PostListItemResponseDto> posts;
     private long totalPages;
     private boolean hasNext;
     private boolean hasPrevious;
@@ -21,12 +22,30 @@ public class PostListWithPageResponseDto {
                 .posts(
                         posts.getContent().stream()
                                 .map(
-                                        PostListResponseDto::from
+                                        PostListItemResponseDto::from
                                 ).toList()
                 )
                 .totalPages(posts.getTotalPages())
                 .hasNext(posts.hasNext())
                 .hasPrevious(posts.hasPrevious())
                 .build();
+    }
+    @Getter
+    @Builder
+    static class PostListItemResponseDto {
+        private final Long id;
+        private final String title;
+        private final LocalDateTime createdAt;
+        private final LocalDateTime updatedAt;
+
+        public static PostListItemResponseDto from(Post entity){
+            return PostListItemResponseDto.builder()
+                    .id(entity.getId())
+                    .title(entity.getTitle())
+                    .createdAt(entity.getCreatedAt())
+                    .updatedAt(entity.getUpdatedAt())
+                    .build();
+        }
+
     }
 }
