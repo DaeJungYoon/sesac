17aac9414ee0555ffc1ffb4552cad48bb31a505a
DaeJungYoon <dbseowjd12@gmail.com>
2025-01-20 09:49:56 +0900

    댓글 작성 구현

--- java/react_my_app/src/pages/PostDetail.jsx
+++ java/react_my_app/src/pages/PostDetail.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import postApi from '../api/postsApi';
+import CommentForm from '../components/CommentForm';
 
 export default function PostDetail() {
   const [post, setPost] = useState({});
@@ -11,12 +12,6 @@ export default function PostDetail() {
 
   useEffect(() => {
     async function fetchPost() {
-      // 게시글 조회 로직
-      //   const response = await postApi.getPostById(postId);
-      //   const data = response.data;
-      //   console.log(data);
-      //   setPost(data)
-      // }
       try {
         const response = await postApi.getPostById(postId);
         setPost(response.data.data);
@@ -30,15 +25,36 @@ export default function PostDetail() {
     fetchPost();
   }, []);
 
-  // if (loading) return <div>로딩중...</div>;
+  if (loading) return <div>로딩중...</div>;
 
+  if (error.status == 404) {
+    return <h3>존재하지 않는 게시글입니다.</h3>;
+  }
   return (
     <div>
-      Post Detail
-      <div>{post.title}</div>
-      <div>{post.content}</div>
-      <div>{post.author}</div>
-      <div>{post.tags}</div>
+      <h3>{post?.title}</h3>
+      <p>{post?.content}</p>
+      <hr />
+      <div>
+        tags :
+        {post?.tags?.map((tag) => {
+          return <span key={tag}>#{tag} </span>;
+        })}
+      </div>
+      <hr />
+      <CommentForm 
+        setPost={setPost}
+        postId={postId}
+      ></CommentForm>
+      {post?.comments?.length ? (
+        <ol>
+          {post?.comments?.map((comment) => {
+            return <li key={`comment-${comment.id}`}>{comment.content}</li>;
+          })}
+        </ol>
+      ) : (
+        <div>댓글이 없습니다.</div>
+      )}
     </div>
   );
 }
